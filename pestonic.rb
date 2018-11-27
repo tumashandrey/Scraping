36ba75f5147cc0255107462cff1bc8e4d15a9b68
tumashandrey <tumashandrey96@gmail.com>
2018-11-27 21:45:34 +0300

    add heders

--- pestonic.rb
+++ pestonic.rb
@@ -1,46 +1,56 @@
 #url = "https://www.petsonic.com/snacks-huesos-para-perros/"
-
+#ruby pestonic "https://www.petsonic.com/snacks-huesos-para-perros/" file.csv
 require 'curb'
 require 'nokogiri'
-require 'uri'
 require 'csv'
 
 class Scraping
+
 	attr_accessor :uri, :file
 
 	def initialize(uri = ARGV[0], file = ARGV[1])
+
 		puts "Scraping, #{ARGV[0]}"
 		@uri = uri
 		@file = file
-		@curl = Curl::Easy.new
+		@curl = Curl::Easy.new do |curl|
+			curl.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36"
+			curl.headers["Accept-Language"] = "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7"
+		end
 		@products = Array.new
+
 	end
 
 	def run
+
 		doc = download_page(@uri)
 		puts "Download: #{ARGV[0]}"
 		puts "We build Nokogiri..."
 		puts "Parsing page 1"
 		parsing_page(doc)
-		(2..3).each do |page|
+		(2..10).each do |page|
 			puts "Parsing page #{page}"
 			doc = download_page("#{ARGV[0]}?p=#{page}")
 			parsing_page(doc)
 		end
 		puts "-" * 80
-		print_products
+		puts "Number products: #{@products.size}"
 		puts "Add result file #{ARGV[1]}"
-		write_to_csv	
+		write_to_csv
+
 	end
 
 	def download_page(url)
+
 		@curl.url = url
 		@curl.perform
 		
 		Nokogiri::HTML(@curl.body_str)
+
 	end
 
 	def parsing_page(doc)
+
 		doc.xpath("//ul[@id='product_list']/li[contains(@class,'ajax_block_product')]//h2/a/@href").each do |product_link|
 
 			page = download_page(product_link)
@@ -61,16 +71,8 @@ class Scraping
 		end
 	end
 
-	def print_products
-		@products.each do |product|
-			puts "Name: #{product[:name]}"
-			puts "Price: #{product[:price]}"
-			puts "Image: #{product[:img]}"
-			puts "-" * 80
-		end
-	end
-
 	def write_to_csv
+
 		CSV.open(@file, "w", write_headers: true, headers: @products.last.keys) do |csv|
 			@products.each do |product|
 				csv << product.values	
@@ -81,8 +83,10 @@ class Scraping
 end
 
 if ARGV.empty?
+
 	puts "ruby pestonic.rb a b. Please, pass the parameters to the script a: link to the category page (any category of the site can be transmitted),
 	b: the name of the file in which the result will be written"
+
 end
 
 Scraping.new.run
